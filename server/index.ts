8ec13faa26d6280dddda4fc5fece83dec55e9d21
Victory-F <victoriia.fuchyzhy@gmail.com>
2022-12-14 11:45:43 +0100

    Installed socket io, created route to create a game

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,51 @@
 import express, { Application, Request, Response } from "express";
+import http from "http";
+import cors from "cors";
 import bodyParser from "body-parser";
+import { Socket, Server } from "socket.io";
+import {
+  Game,
+  GameState,
+  ServerToClientEvents,
+  ClientToServerEvents,
+  SocketData,
+  InterServerEvents,
+} from "../types/gameTypes";
 
+const PORT = process.env.PORT || 4000;
 const app: Application = express();
+const server = http.createServer(app);
+app.use(cors());
+
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Healthy");
-});
+//MAKE IT BETTER!
+const games: { [key: string]: GameState } = {};
 
-const PORT = process.env.PORT || 4000;
+app.get("/game", (req: Request, res: Response) => {
+  const rand = Math.random().toString().slice(2, 8);
+  // Create new game
+  games[rand] = {
+    state: "setup",
+    game: {
+      id: rand,
+      name: "",
+      guide: null,
+      travellers: null,
+      places: null,
+    },
+  };
+  console.log(`New game created with gameId: ${rand}`);
+  res.send({ id: rand });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
--- types/gameTypes.d.ts
+++ types/gameTypes.d.ts
@@ -45,3 +45,21 @@ type Encounter = {
 };
 
 // SOCKET.IO TYPES
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: BufferSource) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
