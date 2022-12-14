a6c5e60c959b13b5a5ab9f3622ae50d7bbdef6ea
Victory-F <victoriia.fuchyzhy@gmail.com>
2022-12-14 14:21:48 +0100

    server test

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from "express";
 import http from "http";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { Socket, Server } from "socket.io";
+import { Server } from "socket.io";
 import {
   Game,
   GameState,
@@ -17,36 +17,36 @@ const app: Application = express();
 const server = http.createServer(app);
 app.use(cors());
 
-const io = new Server<
-  ClientToServerEvents,
-  ServerToClientEvents,
-  InterServerEvents,
-  SocketData
->(server);
+const io = new Server(server);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 //MAKE IT BETTER!
-const games: { [key: string]: GameState } = {};
+// const games: { [key: string]: GameState } = {};
 
-app.get("/game", (req: Request, res: Response) => {
-  const rand = Math.random().toString().slice(2, 8);
-  // Create new game
-  games[rand] = {
-    state: "setup",
-    game: {
-      id: rand,
-      name: "",
-      guide: null,
-      travellers: null,
-      places: null,
-    },
-  };
-  console.log(`New game created with gameId: ${rand}`);
-  res.send({ id: rand });
-});
+// app.get("/game", (req: Request, res: Response) => {
+//   const rand = Math.random().toString().slice(2, 8);
+//   // Create new game
+//   games[rand] = {
+//     state: "setup",
+//     game: {
+//       id: rand,
+//       name: "",
+//       guide: null,
+//       travellers: null,
+//       places: null,
+//     },
+//   };
+//   console.log(`New game created with gameId: ${rand}`);
+//   res.send({ id: rand });
+// });
+
+//Every socket.on and socket.emit needs to be wrapped around "io.on('connection, socket)"
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
+});
+io.on("connection", (socket) => {
+  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
 });
