ac5b32a0a250cec096c3c331ac8fe3a5bb91206d
Victory-F <victoriia.fuchyzhy@gmail.com>
2022-12-27 19:09:00 +0100

    Added styling

--- client/src/styled/BlueLightText.tsx
+++ client/src/styled/BlueLightText.tsx
@@ -4,8 +4,7 @@ import { Title } from "./Title";
 const BlueLightText = styled(Title)`
   margin: 0;
   text-shadow: 0 0 0.8vw #3f26bf, 0 0 1vw #3f26bf, 0 0 1.3vw #3f26bf;
-  &:hover,
-  &:focus {
+  &:hover {
     text-shadow: 0 0 0.8vw #ffd12a, 0 0 1vw #ffd12a, 0 0 1.3vw #ffd12a;
   }
 `;
