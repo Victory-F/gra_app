527ac0644594de1b52db2e25a14f0bc64fd975ad
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-01-05 12:33:44 +0100

    Deleted old api server

--- client/src/config/constants.tsx
+++ client/src/config/constants.tsx
@@ -1,4 +1,2 @@
 export const API_URL =
-  process.env.REACT_APP_SERVER_URL ||
-  "https://gra-app-s.herokuapp.com/" ||
-  "http://localhost:4000/";
+  process.env.REACT_APP_SERVER_URL || "http://localhost:4000/";
